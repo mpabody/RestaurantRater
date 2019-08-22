4212a889995a7f795cc2bc9d1d6d05f93fb05239
Michael Pabody <michael.pabody@gmail.com>
2019-08-21 20:40:46 -0400

    Delete Functionality

--- RestarauntRater_Videos_SecondAttempt/Controllers/RestaurantController.cs
+++ RestarauntRater_Videos_SecondAttempt/Controllers/RestaurantController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -15,6 +16,42 @@ namespace RestarauntRater_Videos_SecondAttempt.Controllers
         public ActionResult Index()
         {
             return View(_db.Restaurants.ToList());
+        }
+
+        // Get: Restaurant/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Restaurant/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Restaurant restaurant)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Restaurants.Add(restaurant);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(restaurant);
+        }
+
+        // GET: Restaurant/Delete/{id}
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Restaurant restaurant = _db.Restaurants.Find(id);
+            if (restaurant == null)
+            {
+                return HttpNotFound();
+            }
+            return View(restaurant);
         }
     }
 }
